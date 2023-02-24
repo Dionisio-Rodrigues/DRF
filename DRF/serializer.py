32109229ccd49c3cbc0da2cfe132c64cf3f49f49
dionisio-rodrigues <d.rodriguesmaianeto@gmail.com>
2023-02-24 16:36:26 -0300

    implementando serializers

--- DRF/serializer.py
+++ DRF/serializer.py
@@ -4,4 +4,19 @@ from DRF.models import Diretor, Professor, Aluno, Cadeira
 class DiretorSerializer(serializers.ModelSerializer):
     class meta:
         model = Diretor
-        
+        fields = ['id', 'nome', 'email', 'cpf', 'endereco', 'status']
+
+class ProfessorSerializer(serializers.ModelSerializer):
+    class meta:
+        model = Professor
+        fields = ['id', 'nome', 'email', 'cpf', 'endereco', 'status', 'cadeiras_cadastradas']
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class meta:
+        model = Aluno
+        fields = ['id', 'nome', 'email', 'cpf', 'endereco', 'status', 'cadeiras_escritas']
+
+class CadeiraSerializer(serializers.ModelSerializer):
+    class meta:
+        model = Cadeira
+        fields = ['id', 'professor', 'horario', 'limite_de_alunos', 'noticias']
